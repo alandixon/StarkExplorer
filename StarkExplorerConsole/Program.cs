ca0b67fe0ad283b1acb9c1bb082da65a7f943b9d
AlanD <alandixon@users.noreply.github.com>
2024-06-17 15:38:07 +0100

    Add project files.

---
+++ StarkExplorerConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StarkExplorerConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
