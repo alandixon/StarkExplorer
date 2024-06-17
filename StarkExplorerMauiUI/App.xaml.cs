314a9c8475f9376af5a7e3819058c7b5e72db2de
AlanD <alandixon@users.noreply.github.com>
2024-06-17 16:47:59 +0100

    Added Maui UI project

---
+++ StarkExplorerMauiUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StarkExplorerMauiUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
