f5768f1c08665e6f34662f18fe3680ac1ec22234
AlanD <alandixon@users.noreply.github.com>
2024-06-19 22:59:18 +0100

    Added request json files and environment var function. Updated README.

--- README.md
+++ README.md
@@ -7,6 +7,17 @@ Three parts:
 * Console program, calls the API library and provides results on the command line
 * GUI program, calls the API library and provides results graphically
 
+## Infura API key
+Stark data is fetched via the [Infura Starknet API](https://docs.infura.io/api/networks/starknet).
+I have used a [free Infura account](https://www.infura.io/pricing) that gives
+* 1 API key
+* 100,000 Total Requests/Day
+* 10 Requests/Second
+
+Which is more than enough for my requirements.
+
+An API key is required and should be stored as an environment variable called ``INFURA_APIKEY`` on the machine that runs StarknetExplorer. This is so that the key does not have to be stored in a software repository, Github in this case. The app will throw an exception if it cannot find ``INFURA_APIKEY``.
+
 
 ## Credits
 
---
+++ StarkExplorerLib/HttpFunctions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace StarkExplorerLib
+{
+    public class HttpFunctions
+    {
+        internal static HttpClient httpClient = new HttpClient();
+
+        static HttpFunctions()
+        {
+            httpClient.BaseAddress = new Uri("https://starknet-sepolia.infura.io/v3/" + Helper.GetEnvVar("INFURA_APIKEY"));
+            httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
+        }
+
+        public async static string GetBlockByNum(int blockNum)
+        {
+            var response = await httpClient.PostAsJsonAsync("Create", json);
+        }
+
+        public async static string GetLastBlockNum()
+        {
+            var response = await httpClient.PostAsJsonAsync("Create", json);
+        }
+
+    }
+}
